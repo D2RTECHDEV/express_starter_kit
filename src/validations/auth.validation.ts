d56faf84573f279806f55c2face0e7bb61cf82c9
Bijoy Haldar <bijoy.haldar999@gmail.com>
2024-10-21 12:51:18 +0530

    feat: Add name field to registration validation

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -3,6 +3,7 @@ import { password } from "./custom.validation.js";
 
 const register = {
   body: Joi.object().keys({
+    name: Joi.string().required(),
     email: Joi.string().required().email(),
     password: Joi.string().required().custom(password),
   }),
